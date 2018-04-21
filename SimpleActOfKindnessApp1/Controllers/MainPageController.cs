078874b268d86743f5c41741e6190f35f0ba3660
Lauri Simonen <lauri.simonen@hotmail.com>
2018-04-21 20:45:53 +0300

    Lisätty etusivu

---
+++ SimpleActOfKindnessApp1/Controllers/MainPageController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SimpleActOfKindnessApp1.Controllers
+{
+    public class MainPageController : Controller
+    {
+        // GET: MainPage
+        public ActionResult MainPage()
+        {
+            return View(); 
+        }
+
+    }
+}
