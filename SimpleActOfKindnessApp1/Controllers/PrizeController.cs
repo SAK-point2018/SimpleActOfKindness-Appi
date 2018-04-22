5a3a59c6b289246e2bdd4dbf88c512f3c8a79764
Lauri Simonen <lauri.simonen@hotmail.com>
2018-04-22 13:22:10 +0300

    Lisätty palkintosivu. Ei muuta vielä kuin peukku.

---
+++ SimpleActOfKindnessApp1/Controllers/PrizeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SimpleActOfKindnessApp1.Controllers
+{
+    public class PrizeController : Controller
+    {
+        // GET: Prize
+        public ActionResult Prize()
+        {
+            return View();
+        }
+    }
+}
