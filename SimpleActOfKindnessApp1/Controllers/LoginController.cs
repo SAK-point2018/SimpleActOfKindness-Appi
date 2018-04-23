42a33634d0448e34d7b73e72dfa57a5910090730
Anne Vaittinen <anne.vaittinen@stud.pointcollege.fi>
2018-04-23 08:53:55 +0300

    Login-kontrolleri ja -näkymä linkitetty toisiinsa ja saatu login-näkymä näkymään selaimessa

--- SimpleActOfKindnessApp1/Controllers/LoginController.cs
+++ SimpleActOfKindnessApp1/Controllers/LoginController.cs
@@ -1,50 +1,17 @@
-﻿//using System;
-//using System.Collections.Generic;
-//using System.Linq;
-//using System.Web;
-//using System.Web.Mvc;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
 
-//namespace SimpleActOfKindnessApp1.Controllers
-//{
-//    public class LoginController : Controller
-//    {
-//        // GET: Login
-//        public ActionResult Index()
-//        {
-//            return View();
-//        }
-//        // https://www.c-sharpcorner.com/article/simple-login-application-using-Asp-Net-mvc/
-
-//        [HttpPost]
-//        [ValidateAntiForgeryToken]
-//        public ActionResult Login(UserProfile objUser)
-//        {
-//            if (ModelState.IsValid)
-//            {
-//                using (DB_Entities db = new DB_Entities())
-//                {
-//                    var obj = db.UserProfiles.Where(a => a.UserName.Equals(objUser.UserName) && a.Password.Equals(objUser.Password)).FirstOrDefault();
-//                    if (obj != null)
-//                    {
-//                        Session["UserID"] = obj.UserId.ToString();
-//                        Session["UserName"] = obj.UserName.ToString();
-//                        return RedirectToAction("UserDashBoard");
-//                    }
-//                }
-//            }
-//            return View(objUser);
-//        }
-
-//        public ActionResult UserDashBoard()
-//        {
-//            if (Session["UserID"] != null)
-//            {
-//                return View();
-//            }
-//            else
-//            {
-//                return RedirectToAction("Login");
-//            }
-//        }
-//    }
-//}
+namespace SimpleActOfKindnessApp1.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        public ActionResult Login()
+        {
+            return View();
+        }
+    }
+}
