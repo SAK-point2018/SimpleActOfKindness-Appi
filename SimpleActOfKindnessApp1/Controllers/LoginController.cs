02053ab095fff384ed16eac2e2e44e105444bc9f
Anne Vaittinen <anne.vaittinen@stud.pointcollege.fi>
2018-04-30 09:15:07 +0300

    Lisätty ConnectionString web.config kansioon

--- SimpleActOfKindnessApp1/Controllers/LoginController.cs
+++ SimpleActOfKindnessApp1/Controllers/LoginController.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Data.SqlClient;
+using System.Data;
+	
 
 namespace SimpleActOfKindnessApp1.Controllers
 {
