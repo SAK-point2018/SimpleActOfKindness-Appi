3cb4a84f079b2cfef123de284fb82142ec3f3d86
Anne Vaittinen <anne.vaittinen@stud.pointcollege.fi>
2018-05-22 09:41:58 +0300

    muokattu profile-kontrolleria ja profile-näkymää, hakee tietoja tietokannasta, mutta ei vielä näytä niitä selaimessa

--- SimpleActOfKindnessApp1/Controllers/ProfileController.cs
+++ SimpleActOfKindnessApp1/Controllers/ProfileController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using SimpleActOfKindnessApp1.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -12,6 +14,24 @@ namespace SimpleActOfKindnessApp1.Controllers
         public ActionResult ProfilePage()
         {
             return View();
+        }
+        //tehtyjen tekojen listaaminen
+        public JsonResult Tehdytteotlista()
+        {
+            ScrumDB2018KEntities entities = new ScrumDB2018KEntities();
+
+            var tehtyteko = (from tt in entities.SAKtehdytteot
+                             select new
+                             {
+                                 Tekopvm = tt.Tekopvm
+                             }).ToList();
+
+            string json = JsonConvert.SerializeObject(tehtyteko);
+            entities.Dispose(); //muistin vapautus
+
+            Response.Expires = -1;
+            Response.CacheControl = "no-cache";
+            return Json(json, JsonRequestBehavior.AllowGet);
         }
     }
 }
