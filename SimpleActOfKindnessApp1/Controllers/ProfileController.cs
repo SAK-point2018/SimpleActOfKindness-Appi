3c9c2a9a2f52cc53a52b3f47d6acf9323deb8db7
Anne Vaittinen <anne.vaittinen@stud.pointcollege.fi>
2018-05-23 11:16:59 +0300

    Hakee tehdyn teon pvm:n profiilisivulle. Teon nimi puuttuu ja ulkoasu huono

--- SimpleActOfKindnessApp1/Controllers/ProfileController.cs
+++ SimpleActOfKindnessApp1/Controllers/ProfileController.cs
@@ -2,6 +2,7 @@
 using SimpleActOfKindnessApp1.Models;
 using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -18,20 +19,25 @@ namespace SimpleActOfKindnessApp1.Controllers
         //tehtyjen tekojen listaaminen
         public JsonResult Tehdytteotlista()
         {
+            //tietokantahaku
             ScrumDB2018KEntities entities = new ScrumDB2018KEntities();
 
-            var tehtyteko = (from tt in entities.SAKtehdytteot
+            var tehtyteko = (from tt in entities.SAKtehdytteot 
                              select new
                              {
-                                 Tekopvm = tt.Tekopvm
+                                 Tekopvm = tt.Tekopvm,
+
                              }).ToList();
 
-            string json = JsonConvert.SerializeObject(tehtyteko);
+            string json = JsonConvert.SerializeObject(tehtyteko); //objekti JSON-muotoon
             entities.Dispose(); //muistin vapautus
 
+            //välimuistin hallinta
             Response.Expires = -1;
             Response.CacheControl = "no-cache";
+            //tulosten palautus
             return Json(json, JsonRequestBehavior.AllowGet);
         }
+
     }
 }
