4e009e66be92f0fa440f8203edadb3ec157aae45
Juha Sonck <juha.sonck@pointcollege.fi>
2018-05-31 18:41:34 +0300

    Juha was here.. eiku: koodattu hieman lunastuskontrollerin tietokantahakua (kommentoitu toimimaton lohko)

--- SimpleActOfKindnessApp1/Controllers/lunastusController.cs
+++ SimpleActOfKindnessApp1/Controllers/lunastusController.cs
@@ -13,38 +13,38 @@ namespace SimpleActOfKindnessApp1.Controllers
         // GET: lunastus
         public ActionResult Index()
         {
-            ScrumDB2018KEntities entities = new ScrumDB2018KEntities();
-            var model = (from tt in entities.SAKtehdytteot
-                         where tt.PalkintoID == 102
-                         select new
-                         {
-                             TehdytTeotID = tt.TehdytTeotID,
-                             KayttajaID = tt.KayttajaID,
-                             Tekopvm = tt.Tekopvm,
-                             PalkintoID = tt.PalkintoID,
-                             VoimassaOloPvm = tt.VoimassaOloPvm,
-                             TekoID = tt.TekoID
-                         }).ToList();
+            //ScrumDB2018KEntities entities = new ScrumDB2018KEntities();
+            //var model = (from tt in entities.SAKtehdytteot
+            //             where tt.PalkintoID == 102
+            //             select new
+            //             {
+            //                 TehdytTeotID = tt.TehdytTeotID,
+            //                 KayttajaID = tt.KayttajaID,
+            //                 Tekopvm = tt.Tekopvm,
+            //                 PalkintoID = tt.PalkintoID,
+            //                 VoimassaOloPvm = tt.VoimassaOloPvm,
+            //                 TekoID = tt.TekoID
+            //             }).ToList();
 
-            //string json = JsonConvert.SerializeObject(model);
-            entities.Dispose();
+            ////string json = JsonConvert.SerializeObject(model);
+            //entities.Dispose();
 
-            //return Json(json, JsonRequestBehavior.AllowGet);
-            return View(model);
-
-            //List<SAKtehdytteot> model = new List<SAKtehdytteot>();
-            //try
-            //{
-            //    ScrumDB2018KEntities entities = new ScrumDB2018KEntities();
-            //    model = entities.SAKtehdytteot.ToList();
-            //    entities.Dispose();
-            //}
-            //catch (Exception ex)
-            //{
-            //    ViewBag.ErrorMessage = ex.GetType() + ": " + ex.Message;
-            //}
-
+            ////return Json(json, JsonRequestBehavior.AllowGet);
             //return View(model);
+
+            List<SAKtehdytteot> model = new List<SAKtehdytteot>();
+            try
+            {
+                ScrumDB2018KEntities entities = new ScrumDB2018KEntities();
+                model = entities.SAKtehdytteot.ToList();
+                entities.Dispose();
+            }
+            catch (Exception ex)
+            {
+                ViewBag.ErrorMessage = ex.GetType() + ": " + ex.Message;
+            }
+
+            return View(model);
         }
     
         public ActionResult getsingleprize()
