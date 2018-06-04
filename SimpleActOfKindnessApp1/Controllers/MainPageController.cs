4fb824d70182f8eb4dda8ca8d6e92e998666a273
Lauri Simonen <lauri.simonen@stud.pointcollege.fi>
2018-06-04 16:51:13 +0300

    Hakee napit SQL:stä muttei vielä tule teon nimeä nappiin.

--- SimpleActOfKindnessApp1/Controllers/MainPageController.cs
+++ SimpleActOfKindnessApp1/Controllers/MainPageController.cs
@@ -17,24 +17,22 @@ namespace SimpleActOfKindnessApp1.Controllers
             return View();
         }
 
-        public JsonResult Lunastamattomatpalkinnot()
+        public JsonResult HyvaTeko()
         {
             ScrumDB2018KEntities entities = new ScrumDB2018KEntities();
 
             CultureInfo fiFI = new CultureInfo("fi-FI");
 
-            var kirjaateko = (from lun in entities.SAKtehdytteot
-                                   join palkinto in entities.SAKpalkinto
-                                   on lun.PalkintoID equals palkinto.PalkintoID
-                                   select new
-                                   {
-                                       palkintonimi = palkinto.PalkintoNimi,
-                                       voimassaolopvm = lun.VoimassaOloPvm,
-                                       palkintoID = lun.PalkintoID
+            var hyvateko = (from teot in entities.SAKteot
 
-                                   }).ToList();
+                            select new
+                            {
+                                TeonNimi = teot.TeonNimi,
+                                TekoID = teot.TekoID
 
-            string json = JsonConvert.SerializeObject(kirjaateko);
+                            }).ToList();
+
+            string json = JsonConvert.SerializeObject(hyvateko);
             entities.Dispose();
 
             Response.Expires = -1;
@@ -50,31 +48,5 @@ namespace SimpleActOfKindnessApp1.Controllers
         }
 
         //tehtyjen tekojen listaaminen
-        public JsonResult Tehdytteotlista()
-        {
-            //tietokantahaku
-            ScrumDB2018KEntities entities = new ScrumDB2018KEntities();
-
-            //SAKteot- ja SAKtehdytteot-taulujen liitos InnerJoinilla
-            //(TeonNimi täytyy hakea SAKteot-taulusta)
-            var innerJoinQuery = (from tehtyteko in entities.SAKtehdytteot
-                                  join teot in entities.SAKteot
-                                  on tehtyteko.TekoID equals teot.TekoID
-                                  select new
-                                  {
-                                      Tekopvm = tehtyteko.Tekopvm,
-                                      TeonNimi = teot.TeonNimi
-
-                                  }).ToList();
-
-            string json = JsonConvert.SerializeObject(innerJoinQuery); //objekti JSON-muotoon
-            entities.Dispose(); //muistin vapautus
-
-            //välimuistin hallinta
-            Response.Expires = -1;
-            Response.CacheControl = "no-cache";
-            //tulosten palautus
-            return Json(json, JsonRequestBehavior.AllowGet);
-        }
     }
 }
