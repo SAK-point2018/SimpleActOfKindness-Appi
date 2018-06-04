fd7bf268de667a26390555b4c1666f2554eb57cf
Lauri Simonen <lauri.simonen@stud.pointcollege.fi>
2018-06-04 15:52:34 +0300

    Muutettu MainPagea siten, että kopioitu Juhan tekemä Cotroller ja ProfilePage ja koitettu muuttaa se MainPagea vastaavaksi. Ei vielä valmis.

--- SimpleActOfKindnessApp1/Controllers/MainPageController.cs
+++ SimpleActOfKindnessApp1/Controllers/MainPageController.cs
@@ -2,47 +2,79 @@
 using SimpleActOfKindnessApp1.Models;
 using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-
 
 namespace SimpleActOfKindnessApp1.Controllers
 {
     public class MainPageController : Controller
     {
-        // GET: MainPage
+        // GET: Profile
         public ActionResult Index()
         {
-            return View(); 
+            return View();
         }
 
-        // GET: Views/Prize
-        public ActionResult Prize()
-        {
-            return RedirectToAction("Prize/Prize");
-        }
-        public JsonResult getlist()
+        public JsonResult Lunastamattomatpalkinnot()
         {
             ScrumDB2018KEntities entities = new ScrumDB2018KEntities();
 
-            var model = (from t in entities.SAKteot
-                         select new
-                         {
-                             TekoID = t.TekoID,
-                             TeonNimi = t.TeonNimi
+            CultureInfo fiFI = new CultureInfo("fi-FI");
 
-                         }).ToList();
+            var kirjaateko = (from lun in entities.SAKtehdytteot
+                                   join palkinto in entities.SAKpalkinto
+                                   on lun.PalkintoID equals palkinto.PalkintoID
+                                   select new
+                                   {
+                                       palkintonimi = palkinto.PalkintoNimi,
+                                       voimassaolopvm = lun.VoimassaOloPvm,
+                                       palkintoID = lun.PalkintoID
 
-            string json = JsonConvert.SerializeObject(model);
+                                   }).ToList();
+
+            string json = JsonConvert.SerializeObject(kirjaateko);
             entities.Dispose();
 
             Response.Expires = -1;
             Response.CacheControl = "no-cache";
 
             return Json(json, JsonRequestBehavior.AllowGet);
+            //return Json(json.AsEnumerable().Select(r => new
+            //{
+            //    voimassaolopvm = r.voimassaolopvm.GetValueOrDefault().ToString("dd.MM.yyyy"),
+            //    palkintonimi = r.palkintonimi
+            //}), JsonRequestBehavior.AllowGet);
 
+        }
 
+        //tehtyjen tekojen listaaminen
+        public JsonResult Tehdytteotlista()
+        {
+            //tietokantahaku
+            ScrumDB2018KEntities entities = new ScrumDB2018KEntities();
+
+            //SAKteot- ja SAKtehdytteot-taulujen liitos InnerJoinilla
+            //(TeonNimi täytyy hakea SAKteot-taulusta)
+            var innerJoinQuery = (from tehtyteko in entities.SAKtehdytteot
+                                  join teot in entities.SAKteot
+                                  on tehtyteko.TekoID equals teot.TekoID
+                                  select new
+                                  {
+                                      Tekopvm = tehtyteko.Tekopvm,
+                                      TeonNimi = teot.TeonNimi
+
+                                  }).ToList();
+
+            string json = JsonConvert.SerializeObject(innerJoinQuery); //objekti JSON-muotoon
+            entities.Dispose(); //muistin vapautus
+
+            //välimuistin hallinta
+            Response.Expires = -1;
+            Response.CacheControl = "no-cache";
+            //tulosten palautus
+            return Json(json, JsonRequestBehavior.AllowGet);
         }
     }
 }
